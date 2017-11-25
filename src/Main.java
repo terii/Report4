0cd9a82953dabf838d74a3515b7cfbe340258d95
Seiryu Hidaka <e175726@ie.u-ryukyu.ac.jp>
2017-11-25 19:35:33 +0900

    step2 NullpointerExxception error try-catch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
         public static void main(String[] args){
-            String str=null;
-            System.out.println(str.length());
+            try {
+                String str = null;
+                System.out.println(str.length());
+            }catch(NullPointerException e){
+                System.err.println("NullPointerException error "+e.getMessage());
+            }
         }
     }
