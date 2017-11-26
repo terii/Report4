72d3a51f3f3913a4a6af9aefd24cd099eaef1c4e
Seiryu Hidaka <e175726@ie.u-ryukyu.ac.jp>
2017-11-26 14:25:34 +0900

    step3 NumberFormatException 3．14　error

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@
 public class Main {
         public static void main(String[] args){
             try {
-                String str = null;
-                System.out.println(str.length());
+                String str ="3．14";
+                double value;
+                value=Double.parseDouble(str);
+                System.out.println(value);
             }catch(NullPointerException e){
                 System.err.println("NullPointerException error "+e.getMessage());
+            }catch(NumberFormatException e){
+                System.err.println("NumberFormatException error "+e.getMessage());
             }
         }
     }
